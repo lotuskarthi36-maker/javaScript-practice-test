8da82626e9ddee49628f874e1267999d8adf32f5
lotuskarthi36-maker <lotuskarthi36@gmail.com>
2025-11-09 20:59:42 +0530

    Add student permission checks in task3.js

---
+++ task3/task3.js
@@ -0,0 +1,11 @@
+const student = true;
+const ID = false;
+if(student && ID){
+    console.log("student allow to  the lab");
+}
+if(student || ID){
+    console.log("student allow to  the lab");
+}
+if(!student){
+    console.log("not permited");
+}
